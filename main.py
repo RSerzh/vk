729e1bd04b0a7a357bba52da8d8054ac2d8d493f
xyzm <xyzm@list.ru>
2020-02-10 20:08:28 +0300

    Запускаю набросок парсера. Смотрю что получается.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import vk_api as vk
 import time
 
+scan_domain('the_riddler_2k17')
+
 #print( vk.get_posts("the_riddler_2k17") )
 
 #psts = vk.get_cnt_posts("the_riddler_2k17")
@@ -11,21 +13,22 @@ import time
 #     #time.sleep(1)
 #     print(i)
 
-arr_p = vk.get_posts("the_riddler_2k17",3,0)
-
-i = arr_p['items'][0]
-print([key for key in i])
+# cnt = vk.get_cnt_posts("the_riddler_2k17")
+# arr_p = vk.get_posts("the_riddler_2k17",1,0)
+# print("Постов=",cnt)
+# i = arr_p['items'][0]
+# print([key for key in i])
 #print(time.ctime( i['date'] ) )
 
-print('--------------------------')
-print('Id=',i['id'])
-print('Text=',i['text'])
-print(i['attachments'])
-print(i['copy_history'])
+# print('--------------------------')
+# print('Id=',i['id'])
+# print('Text=',i['text'])
+#print(i['attachments'])
+#print(i['copy_history'])
 
 
-for i in arr_p['items']:
+# for i in arr_p['items']:
     #print([key for key in i])
     #print( i['id'] , i['text'])
-    pass
+    # pass
 
--- vk_api.py
+++ vk_api.py
@@ -1,8 +1,38 @@
 import sys
 sys.path.append(r"C:\Program Files\Python38\Lib\site-packages")
 import requests
+import time
 
 token = '4bdb3528b751be13b6df2d41fcdc28823a7e9457cedb19ffecfdc0b4de0685f8d0c9590c103dac70421ae'
+
+def get_my_format_time(sec):
+    return time.strftime("%d.%m.%y %H:%M:%S",time.localtime(sec))
+
+def scan_domain(domain):
+    cnt_posts = get_cnt_posts(domain)
+    print("Постов",cnt_posts)
+
+    debug=True
+
+    if(debug):
+        arr_p = get_posts(domain,1,0)
+        items = arr_p['items']
+        print( items )
+    if(debug):
+        return
+
+    for i in range(0,cnt_posts,100):
+        arr_p = get_posts(domain,100,i)
+        items = arr_p['items']
+        time.sleep(1)
+        for j in items:
+            print( 'id=', j['id'] , 'date=', get_my_format_time(j['date']) )
+            if 'text' in j:
+                print( 'text=' , j['text'])
+            if 'attachments' in j:
+                print( j['attachments']['title'] )
+        print('---Step---',i,'items=',len(items))
+
 
 def get_posts(domain,cnt=1,offset=0):
     params={'access_token': token,'v': '5.103','domain':domain,'count':cnt,'offset':offset }
@@ -28,4 +58,4 @@ params={
 #url = 'https://api.vk.com/method/groups.getById'
 #url = 'https://api.vk.com/method/wall.get'
 
-get_cnt_posts("the_riddler_2k17")
+scan_domain('the_riddler_2k17')
